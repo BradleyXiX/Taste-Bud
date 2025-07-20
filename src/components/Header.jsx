83245306c5f30225831470196a0453cb31f61e86
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-07-20 13:19:58 +0300

    started tailwind frontend redesign

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="header">
+        <header className="flex w-full items-center justify-center gap-5.5">
             <img src="/Chef Claude Icon.png" className="header-icon" />
             <h1>Taste Bud</h1>
         </header>
